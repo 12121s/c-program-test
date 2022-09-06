9b38cc8ae1d265761a7988108bd3d6f775b2c072
mr_kim <rofalfl42@gmail.com>
2022-09-06 23:23:55 +0900

    **수정내역**: 1. getTime 함수 수정 (데이터 세팅하는 코드 주석 제거) **프로젝트** : CLogTest **작성자** : illi

--- Timer.c
+++ Timer.c
@@ -1,17 +1,18 @@
-
-#include <time.h>
 #include <assert.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 char getTime(char date[10]){
 	time_t timer;
 	struct tm* t;
 
+	// 시간을 구해와서 t에 저장
 	timer = time(NULL);
-
 	t = localtime(&timer);
 
+	// 구해온 int type의 사간을 char 타입으로 변환해서 저장하기 위한 공간
 	char* years[4];
 	char* months[2];
 	char* days[2];
@@ -21,14 +22,13 @@ char getTime(char date[10]){
 	memset(months, 0x00, sizeof(months));
 	memset(days, 0x00, sizeof(days));
 
-
 	char year[10];
 	char month[10];
 	char day[10];
 
-	// sprintf(year, "%d", t->tm_year + 1900 - 2000);
-	// sprintf(month, "%d", t->tm_mon + 1);
-	// sprintf(day, "%d", t->tm_mday);
+	sprintf(year, "%d", t->tm_year + 1900 - 2000);
+	sprintf(month, "%d", t->tm_mon + 1);
+	sprintf(day, "%d", t->tm_mday);
 
 	strncpy(years, year, 2);
 	strncpy(months, month, 2);
